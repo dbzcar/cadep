e8e53d0e1396dfb83b8fa40a5ca6f66135bfed7b
dbzcar <cardx00@gmail.com>
2016-09-30 22:53:56 -0600

    Update models.py

--- app/municipio/models.py
+++ app/municipio/models.py
@@ -7,6 +7,7 @@ from django.db import models
 # Create your models here.
 	
 class Municipio(models.Model):
+	codigo = models.PositiveIntegerField(primary_key=True)
 	nombre = models.CharField(max_length=50)
     #departamento= models.ForeignKey(Departamento, null=True, blank=True, on_delete=models.CASCADE)
 	
